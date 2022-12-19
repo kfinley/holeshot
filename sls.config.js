b904c3da9e1a0155cb22a3d2e2d7ffbae009f73d
Kyle Finley <kyle@kylefinley.net>
2022-12-19 17:20:33 +0000

    update: more version hell

--- sls.config.js
+++ sls.config.js
@@ -3,7 +3,6 @@ const slsw = require('serverless-webpack');
 module.exports = {
     context: __dirname,
     mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-    scriptPath: `${__dirname}/scripts`,
     servicesPath: `./services`,
     services: ['User', 'WebSockets'],
 };
