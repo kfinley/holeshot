c31320a2f2b8a1122338033074de91d2d1ab5a26
Kyle Finley <kyle@kylefinley.net>
2023-01-10 22:45:04 +0000

    update: config fix

--- packages/vue2-client/src/config.ts
+++ packages/vue2-client/src/config.ts
@@ -13,10 +13,10 @@ config.ClientId = jsConfig.ClientId ?? import.meta.env.VITE_APP_CLIENT_ID!
 config.PoolId = jsConfig.PoolId ?? import.meta.env.VITE_APP_POOL_ID!
 config.Api = jsConfig.Api ?? import.meta.env.VITE_APP_API!
 config.ApiPorts = jsConfig.ApiPorts ?? import.meta.env.VITE_APP_API_PORTS!
-config.ServiceWorkerPath = import.meta.env.VITE_APP_SERVICE_WORKER_PATH!
-config.Host = import.meta.env.VITE_APP_HOST!
-config.Agent = import.meta.env.VITE_APP_AGENT!
-config.WebSocket = import.meta.env.VITE_APP_WEBSOCKET!
-config.WebSocketPort = import.meta.env.VITE_APP_WEBSOCKET_PORT!
+config.ServiceWorkerPath = jsConfig.ServiceWorkerPath ?? import.meta.env.VITE_APP_SERVICE_WORKER_PATH!
+config.Host = jsConfig.Host ?? import.meta.env.VITE_APP_HOST!
+config.Agent = jsConfig.Agent ?? import.meta.env.VITE_APP_AGENT!
+config.WebSocket = jsConfig.WebSocket ?? import.meta.env.VITE_APP_WEBSOCKET!
+config.WebSocketPort = jsConfig.WebSocketPort ?? import.meta.env.VITE_APP_WEBSOCKET_PORT!
 
 console.log(config)
