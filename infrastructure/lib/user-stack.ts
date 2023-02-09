80feeabb7ec7ff0561067826d1e27733bd378bef
Kyle Finley <kyle@kylefinley.net>
2023-02-09 17:34:17 +0000

    update: fixed wrong name for confirmation template

--- infrastructure/lib/user-stack.ts
+++ infrastructure/lib/user-stack.ts
@@ -128,7 +128,7 @@ export class UserServiceStack extends Construct {
     const sendConfirmation = newLamda('SendConfirmation', 'functions/sendConfirmation.handler', {
       HOLESHOT_SITE_URL: props?.siteUrl!,
       HOLESHOT_SENDER_EMAIL: props?.senderEmail!,
-      HOLESHOT_CONFIRMATION_EMAIL_TEMPLATE: 'ConfirmationRegistration'
+      HOLESHOT_CONFIRMATION_EMAIL_TEMPLATE: 'ConfirmRegistration'
     });
 
     const lambdaSesSendTemplatedEmailPolicy = new Policy(this, 'lambdaSesSendTemplatedEmailPolicy');
