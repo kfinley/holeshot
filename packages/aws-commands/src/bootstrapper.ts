2bb54bf71613372535d97acbb2e6acdac8d33fa8
Kyle Finley <kyle@kylefinley.net>
2023-02-19 16:39:14 +0000

    update: pulling s3client config from env vars

--- packages/aws-commands/src/bootstrapper.ts
+++ packages/aws-commands/src/bootstrapper.ts
@@ -80,10 +80,17 @@ export default function bootstrapper(container: Container) {
   }
 
   if (!container.isBound("S3Client")) {
+
     container.bind<S3Client>("S3Client")
       .toDynamicValue(() => process.env.NODE_ENV === 'production'
         ?
-        new S3Client({ }) // Prod
+        new S3Client({
+          region: process.env.AWS_REGION,
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+          }
+        }) // Prod
         :
         new S3Client({ // Local Dev
           region: "us-east-1",
