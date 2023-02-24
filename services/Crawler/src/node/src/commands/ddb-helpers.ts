90e79974fe5a158ecd0fc9f4d0cddc9ac0d36e9d
Kyle Finley <kyle@kylefinley.net>
2023-02-24 13:59:58 -0800

    update: trackInfo mapping fix

--- services/Crawler/src/node/src/commands/ddb-helpers.ts
+++ services/Crawler/src/node/src/commands/ddb-helpers.ts
@@ -1,103 +1,6 @@
 import { AttributeValue } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
-import { TrackInfo, Location, Address, Event } from "@holeshot/types/src";
-
-function addWithKeyIfPropHasValue(attributes: Record<string, AttributeValue>, property: string, value: string | undefined) {
-  if (value) {
-    attributes[property] = {
-      S: value
-    }
-  }
-}
-
-function convertUserOptionalAttributesToItem(track: TrackInfo) {
-  let attributes: Record<string, AttributeValue> = {};
-
-  // addWithKeyIfPropHasValue(attributes, 'extendedDetails', user.extendedDetails);
-  // addWithKeyIfPropHasValue(attributes, 'appearsOnStatementAs', user.appearsOnStatementAs);
-  // addWithKeyIfPropHasValue(attributes, 'address', user.address);
-  // addWithKeyIfPropHasValue(attributes, 'city', user.city);
-  // addWithKeyIfPropHasValue(attributes, 'state', user.state);
-  // addWithKeyIfPropHasValue(attributes, 'postalCode', user.postalCode);
-  // addWithKeyIfPropHasValue(attributes, 'country', user.country);
-  // addWithKeyIfPropHasValue(attributes, 'reference', user.reference);
-  // addWithKeyIfPropHasValue(attributes, 'category', user.category);
-
-  return attributes;
-}
-
-export function dictToMap(dict: Record<string, string>) {
-  let result = {};
-
-  for (let k in dict) {
-    result[k] = {
-      S: dict[k]
-    }
-  }
-  return result;
-}
-
-export function addressToMap(address: Address) {
-  return {
-    line1: {
-      S: address.line1
-    },
-    line2: {
-      S: address.line2
-    },
-    city: {
-      S: address.city
-    },
-    state: {
-      S: address.state
-    },
-    postalcode: {
-      S: address.postalCode
-    }
-  }
-}
-
-export function locationToMap(location: Location) {
-  return {
-    address: {
-      M: addressToMap(location.address)
-    },
-    mapLink: {
-      S: location.mapLink
-    },
-    gps: {
-      M: {
-        Lat: {
-          S: location.gps.lat
-        },
-        long: {
-          S: location.gps.long
-        }
-      }
-    }
-  };
-}
-
-export function operatorsToMap(operators: string[]) {
-
-  let result = {}
-
-  for (let i = 0; i < operators.length; i++) {
-    if (operators[i].indexOf('@') == -1) {
-      const opKey = 'Operator' + (i == 0 ? '1' : '2'); // Kludge...
-      result[opKey] = {
-        S: `${operators[i]} ${operators[i + 1]}`
-      }
-      i += 1;
-    } else {
-      result['Email'] = {
-        S: operators[i]
-      }
-    }
-  }
-  return result;
-
-}
+import { TrackInfo, Event } from "@holeshot/types/src";
 
 export function convertTrackInfoToItem(ownerId: string, track: TrackInfo): {
   [key: string]: AttributeValue;
@@ -120,43 +23,7 @@ export function convertTrackInfoToItem(ownerId: string, track: TrackInfo): {
     type: {
       S: 'Track'
     },
-    id: {
-      S: track.id
-    },
-    name: {
-      S: track.name
-    },
-    district: {
-      S: track.district
-    },
-    dateCreated: {
-      S: created
-    },
-    contactInfo: {
-      M: dictToMap(track.contactInfo)
-    },
-    logoUrl: {
-      S: track.logoUrl
-    },
-    location: {
-      M: locationToMap(track.location)
-    },
-    webSite: {
-      S: track.website
-    },
-    socials: {
-      M: dictToMap(track.socials)
-    },
-    sponsors: {
-      M: dictToMap(track.sponsors)
-    },
-    coaches: {
-      M: dictToMap(track.coaches)
-    },
-    operators: {
-      M: operatorsToMap(track.operators as string[])
-    }
-    // ...optionalAttributes
+    ...marshall(track)
   }
 }
 
