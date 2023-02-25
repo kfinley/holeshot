15a0b291dd27346d2c2d726348f40538f0694d29
Kyle Finley <kyle@kylefinley.net>
2023-02-25 21:15:46 +0000

    update: python lambda path fix

--- infrastructure/lib/crawler-service.ts
+++ infrastructure/lib/crawler-service.ts
@@ -75,8 +75,8 @@ export class CrawlerService extends Construct {
 
     const decodeEmailsLambda = new LambdaFunction(this, 'Holeshot-DecodeEmailsFunction', {
       functionName: 'Holeshot-DecodeEmails',
-      code: Code.fromAsset('../services/Crawler/src/python', { exclude: ["**", "!functions/decode_emails.py", "!commands/decode_emails.py"] }),
-      handler: 'decode-emails.handler',
+      code: Code.fromAsset('../services/Crawler/src/python', { exclude: ["tests", "__pycache__", "!functions", "!commands", "!functions/decode_emails.py", "!commands/decode_emails.py"] }),
+      handler: 'functions.decode-emails.handler',
       runtime: Runtime.PYTHON_3_8,
       environment: {
         BUCKET_NAME: `${props!.domainName}-crawler`,
