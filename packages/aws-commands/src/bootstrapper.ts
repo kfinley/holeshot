58266a2654357f30fbf89ee738b093de96d51b9f
Kyle Finley <kyle@kylefinley.net>
2023-03-01 20:37:37 +0000

    update: Get events nearby wip - set endpoint again

--- packages/aws-commands/src/bootstrapper.ts
+++ packages/aws-commands/src/bootstrapper.ts
@@ -50,7 +50,8 @@ export default function bootstrapper(container: Container) {
         process.env.NODE_ENV === 'production'
           ?
           new DynamoDBClient({
-            region: process.env.AWS_REGION
+            region: process.env.AWS_REGION,
+            endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
           }) // Prod
           :
           new DynamoDBClient({ // Local Dev
