46588f146f8b945cee15ab17e305e30cb4d0cae4
Kyle Finley <kyle@kylefinley.net>
2023-03-02 00:00:56 +0000

    update: Get events nearby wip - cleanup

--- packages/vue2-user/src/commands/setPassword.ts
+++ packages/vue2-user/src/commands/setPassword.ts
@@ -2,30 +2,29 @@ import { SetPasswordRequest, SetPasswordResponse } from '../types';
 import { Command } from '@holeshot/commands/src';
 import { Inject, injectable } from 'inversify-props';
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
-import { config } from "@holeshot/web-core/src/config";
+import { config } from '@holeshot/web-core/src/config';
 
 @injectable()
-export class SetPasswordCommand implements Command<SetPasswordRequest, SetPasswordResponse> {
-
+export class SetPasswordCommand
+  implements Command<SetPasswordRequest, SetPasswordResponse>
+{
   @Inject('CognitoIdentityProvider')
   private provider!: CognitoIdentityProvider;
 
   public async runAsync(params: SetPasswordRequest): Promise<SetPasswordResponse> {
-
     if (params.session !== undefined) {
-
       const request = {
         ClientId: config.ClientId,
-        ChallengeName: "NEW_PASSWORD_REQUIRED",
+        ChallengeName: 'NEW_PASSWORD_REQUIRED',
         ChallengeResponses: {
           USERNAME: params.username,
           NEW_PASSWORD: params.proposedPassword,
         },
-        Session: params.session
-      }
+        Session: params.session,
+      };
 
       try {
-        var result = await this.provider.respondToAuthChallenge(request);
+        const result = await this.provider.respondToAuthChallenge(request);
         if (result.AuthenticationResult) {
           return {
             success: true,
@@ -34,9 +33,9 @@ export class SetPasswordCommand implements Command<SetPasswordRequest, SetPasswo
               expiresIn: result.AuthenticationResult.ExpiresIn,
               tokenType: result.AuthenticationResult.TokenType,
               refreshToken: result.AuthenticationResult.RefreshToken,
-              idToken: result.AuthenticationResult.IdToken
+              idToken: result.AuthenticationResult.IdToken,
             },
-          }
+          };
         }
       } catch (e: any) {
         console.log('setPassword Error: ', e);
@@ -46,8 +45,7 @@ export class SetPasswordCommand implements Command<SetPasswordRequest, SetPasswo
 
     return {
       success: false,
-      error: "Failed to set password"
+      error: 'Failed to set password',
     };
   }
 }
-
