8132b36e1b3577bd0812cd5efcedc56aadb61783
Kyle Finley <kyle@kylefinley.net>
2023-03-03 22:01:11 +0000

    update: Get events nearby wip - added defaultContainer for inversify

--- services/Core/src/inversify.config.ts
+++ services/Core/src/inversify.config.ts
@@ -1,6 +1,7 @@
 // Loaded once per application. Required for dependency injection
 import 'reflect-metadata';
 import { Container } from "inversify-props";
+import { container as defaultContainer } from 'inversify-props';
 
 const container = new Container({
   autoBindInjectable: true,
