63203c8127765986b52b76fa9b845e77696956f0
Kyle Finley <kyle@kylefinley.net>
2023-03-15 20:28:56 +0000

    update: Get events nearby wip - removed dummy data

--- packages/holeshot-plugin/src/components/event-search.vue
+++ packages/holeshot-plugin/src/components/event-search.vue
@@ -146,43 +146,43 @@ export default class EventSearch extends BaseControl {
     this.state.searchResults = null;
 
     // Example of lazy local testing... yes it's lame but quick and easy. :)
-    this.state.searchResults = {
-      searched: 1,
-      tracks: [
-        {
-          id: "4680c24d-14e3-4440-83e0-662d07b5c4f9",
-          name: "Rock Hill BMX Supercross Track",
-          district: "SC01",
-          location: {
-            address: {
-              line1: "1307 Riverwalk Parkway",
-              line2: "",
-              city: "Rock Hill",
-              state: "SC",
-              zipCode: "29730",
-            },
-          },
-        },
-      ],
-      events: [
-        {
-          id: "e3e291cc-76ef-4b77-9238-7797936468af",
-          name: "Practice",
-          trackName: "Rock Hill BMX Supercross Track",
-          date: "2023-03-02T00:00:00",
-          url: "www.usabmx.com/tracks/1971/events/493331",
-          eventType: "Practice",
-          details: {
-            registrationStart: "4:00 PM",
-            registrationEnd: "8:00 PM",
-            raceTime: "4:00 PM",
-            type: "Practice",
-            description: "$5 USA BMX Practice 4-8PM",
-            fee: "$5.00 USD",
-          },
-        },
-      ],
-    };
+    // this.state.searchResults = {
+    //   searched: 1,
+    //   tracks: [
+    //     {
+    //       id: "4680c24d-14e3-4440-83e0-662d07b5c4f9",
+    //       name: "Rock Hill BMX Supercross Track",
+    //       district: "SC01",
+    //       location: {
+    //         address: {
+    //           line1: "1307 Riverwalk Parkway",
+    //           line2: "",
+    //           city: "Rock Hill",
+    //           state: "SC",
+    //           zipCode: "29730",
+    //         },
+    //       },
+    //     },
+    //   ],
+    //   events: [
+    //     {
+    //       id: "e3e291cc-76ef-4b77-9238-7797936468af",
+    //       name: "Practice",
+    //       trackName: "Rock Hill BMX Supercross Track",
+    //       date: "2023-03-02T00:00:00",
+    //       url: "www.usabmx.com/tracks/1971/events/493331",
+    //       eventType: "Practice",
+    //       details: {
+    //         registrationStart: "4:00 PM",
+    //         registrationEnd: "8:00 PM",
+    //         raceTime: "4:00 PM",
+    //         type: "Practice",
+    //         description: "$5 USA BMX Practice 4-8PM",
+    //         fee: "$5.00 USD",
+    //       },
+    //     },
+    //   ],
+    // };
   }
 
   get searching() {
