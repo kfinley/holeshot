95882e64b5d6087f74d65a85d076aade830b853e
Kyle Finley <kyle@kylefinley.net>
2023-03-15 00:33:43 +0000

    update: Get events nearby wip - moved bootstrap to cdn

--- packages/vue2-client/src/plugins/client.ts
+++ packages/vue2-client/src/plugins/client.ts
@@ -20,8 +20,6 @@ import { getModule } from 'vuex-module-decorators';
 import UserModule from '@holeshot/vue2-user/src/store/user-module';
 import { SearchModule } from '@holeshot/plugin/src/store/search-store';
 
-//Move these maybe??
-import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/styles.scss';
 
 export const setupModules = (store: Store<any>): void => {
