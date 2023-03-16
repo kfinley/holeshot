4765abbaf28ee47b13e723b2f4ae5e834e5e0679
Kyle Finley <kyle@kylefinley.net>
2023-03-16 01:41:05 +0000

    update: removing services...

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -275,28 +275,35 @@ export class InfrastructureStack extends Stack {
       certificate: this.certificate
     });
 
-    const schedulerService = new SchedulerService(this, 'Holeshot-SchedulerService', {
-      domainName,
-      coreTable: dataStores?.coreTable,
-      node_env: props!.node_env,
-      sendMessageStateMachine: webSocketsApi.sendMessageStateMachine
-    });
 
-    const lambdaInvokePolicy = new Policy(this, 'Holeshot-Inline-LambdaInvokePolicy');
-    lambdaInvokePolicy.addStatements(
-      new PolicyStatement({
-        actions: [
-          "lambda:InvokeFunction"
-        ],
-        effect: Effect.ALLOW,
-        resources: [
-          schedulerService.getNearbyEvents.functionArn,
-          crawlerService.getTracksForRegion.functionArn
-        ]
-      })
-    );
+    
+    // const schedulerService = new SchedulerService(this, 'Holeshot-SchedulerService', {
+    //   domainName,
+    //   coreTable: dataStores?.coreTable,
+    //   node_env: props!.node_env,
+    //   sendMessageStateMachine: webSocketsApi.sendMessageStateMachine
+    // });
 
-    webSocketsApi.messageHandler.role?.attachInlinePolicy(lambdaInvokePolicy);
+    // const lambdaInvokePolicy = new Policy(this, 'Holeshot-Inline-LambdaInvokePolicy');
+    // lambdaInvokePolicy.addStatements(
+    //   new PolicyStatement({
+    //     actions: [
+    //       "lambda:InvokeFunction"
+    //     ],
+    //     effect: Effect.ALLOW,
+    //     resources: [
+    //       schedulerService.getNearbyEvents.functionArn,
+    //       crawlerService.getTracksForRegion.functionArn
+    //     ]
+    //   })
+    // );
+
+    // webSocketsApi.messageHandler.role?.attachInlinePolicy(lambdaInvokePolicy);
+
+
+
+
+
 
     // new CfnOutput(this, 'apiEndpoint', {
     //   value: `${webSocketsApi.webSocketApi.apiId}.execute-api.${region}.amazonaws.com`,
